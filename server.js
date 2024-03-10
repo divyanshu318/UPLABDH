50d175a717e4b6b720a1e3114eb13ad4d7313593
Bissa <divyanshubissa99@gmail.com>
2024-03-10 13:36:53 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get("/",(req,res)=>{
+    res.send("<h1>UPLABDH</h1>");
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`.bgCyan.white);
+});
