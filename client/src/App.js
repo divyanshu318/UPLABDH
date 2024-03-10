0a3809d4c7e1df40b546ec886db1097c1b802264
Bissa <divyanshubissa99@gmail.com>
2024-03-10 17:53:55 +0530

    signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Layout from './components/layout/Layout.js';
 import HomePage from './pages/HomePage.js';
 import PageNotFound from './pages/PageNotFound.js';
+import SignUp from './pages/SignUp.js';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path = '/' element = {<HomePage/>}/>
+        <Route path = '/signUp' element = {<SignUp/>}/>
         <Route path = '*' element = {<PageNotFound/>}/>
       </Routes>
     </>
---
+++ client/src/pages/SignUp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const SignUp = () => {
+  return (
+    <Layout>SignUp</Layout>
+  )
+}
+
+export default SignUp
